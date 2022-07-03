3afdc33f41bf0f7b0ad2f604375c81f54b8b4ad6
shohruzbek <shohruzbekochilov007@gmail.com>
2022-07-03 23:53:39 +0500

    socket.io client

--- src/components/common_components/Badge/index.jsx
+++ src/components/common_components/Badge/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Badge from '@mui/material/Badge';
 import Tooltip from '@mui/material/Tooltip';
 import Popover from '@mui/material/Popover';
@@ -6,7 +6,13 @@ import MailIcon from '@mui/icons-material/Mail';
 import IconButton from "@mui/material/IconButton";
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { StyleNotification, StyleNotificationBody, StyleNotificationFooter, StyleNotificationHeader } from "./styles";
+import { io } from "socket.io-client";
+import {
+  StyleNotification,
+  StyleNotificationBody,
+  StyleNotificationFooter,
+  StyleNotificationHeader
+} from "./styles";
 
 export default function SimpleBadge() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -21,6 +27,11 @@ export default function SimpleBadge() {
 
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
+  
+  useState(() => {
+    const socket = io("http://localhost:8080");
+    socket.emit('add-user',12)
+  },[])
 
   return (
     <div>
